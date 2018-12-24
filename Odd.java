6e2cce418819fda3091eb60f991dcda763a4276c
Prayas Jain <jain.prayas78@gmail.com>
2018-12-24 22:38:52 +0530

    Odd.java

---
+++ Odd.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Odd{
+public static void main(String arg[]){
+Scanner sc=new Scanner(System.in);
+int num=sc.nextInt();
+if(num%2==0){
+System.out.println("Square : "+num*num);
+
+}
+if(num%2==1){
+System.out.println("Cube : "+num*num*num);
+
+}
+
+}
+
+}
